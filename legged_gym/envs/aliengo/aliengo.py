5f4574f2064fb4f25b482d4a334c1cb4b78cda06
Jeremiah Coholich <jcoholich@gatech.edu>
2023-01-10 15:25:05 -0500

    added Aliengo env with bugs

---
+++ legged_gym/envs/aliengo/aliengo.py
@@ -0,0 +1,6 @@
+from legged_gym.envs import LeggedRobot
+# from legged_gym import LEGGED_GYM_ROOT_DIR
+# from .mixed_terrains.anymal_c_rough_config import AnymalCRoughCfg
+
+class Aliengo(LeggedRobot):
+    pass
